21a88be326f8db62c2835b1bc6cd30d8b40c7c08
Tamara Sibileva <sibilevato@gmail.com>
2025-05-17 22:52:03 +1200

    Tasks changed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,30 +1,30 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-	let arr = [];
-	let d = Math.pow(b, 2) - 4 * a * c;
+  let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+  
+  if (d < 0) {
+    arr = [];
+  }
 
-	if (d < 0) {
-		arr = [];
-	}
-
-	if (d === 0) {
-		arr = [-b / (2 * a)];
-	}
-
-	if (d > 0) {
-		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
-	}
-
-	return arr;
+  if (d === 0) {
+    arr = [-b / (2 * a)];
+  }
+  
+  if (d > 0) {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
+  
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let annualRate = percent / 100;
-	let monthlyRate = annualRate / 12;
-	let credit = amount - contribution;
-	let monthlyPayment = credit * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
-	let payment = Number((monthlyPayment * countMonths).toFixed(2));
-
-	return payment;
+  let annualRate = percent / 100;
+  let monthlyRate = annualRate / 12;
+  let credit = amount - contribution;
+  let monthlyPayment = credit * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+  let payment = Number((monthlyPayment * countMonths).toFixed(2));
+  
+  return payment;
 }
