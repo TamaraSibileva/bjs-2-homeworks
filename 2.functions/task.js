d7cadecc6218a0b181cc5b485aac9e629531a2c3
Tamara Sibileva <sibilevato@gmail.com>
2025-05-20 11:06:07 +1200

    Task1 completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,21 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    
+    sum += arr[i];
+  }
+  
+  const avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
