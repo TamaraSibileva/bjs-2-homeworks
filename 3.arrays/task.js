cfaa928b180775718f54ee4c9cc221ca5679168c
Tamara Sibileva <sibilevato@gmail.com>
2025-05-23 11:18:48 +1200

    Task1 completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,12 @@
+// Task#1: Comparing arrays:
 function compareArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   
+  return arr1.every((item, index) => {
+    return item === arr2[index];
+  });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
