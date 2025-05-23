282e33237c41ff5c2d7a57aff93724dab2260b97
Tamara Sibileva <sibilevato@gmail.com>
2025-05-23 12:14:10 +1200

    Task2 completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -9,6 +9,15 @@ function compareArrays(arr1, arr2) {
   });
 }
 
+// Task2: Filtering and converting an array:
 function getUsersNamesInAgeRange(users, gender) {
+  const filteredUsers = users.filter(user => user.gender === gender);
   
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  const sumAge = filteredUsers.reduce((acc, user) => acc + user.age, 0)
+  
+  return sumAge / filteredUsers.length;
 }
