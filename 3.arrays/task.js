173b951ee9e0599860dda73e5b4637c6f324c3d9
Tamara Sibileva <sibilevato@gmail.com>
2025-05-26 10:33:25 +1200

    Task2 changed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -11,13 +11,17 @@ function compareArrays(arr1, arr2) {
 
 // Task2: Filtering and converting an array:
 function getUsersNamesInAgeRange(users, gender) {
-  const filteredUsers = users.filter(user => user.gender === gender);
+  return users.filter(user => user.gender === gender)
+    .map(user => user.age)
+    .reduce((acc, age, index, arr) => acc + age / arr.length, 0);
   
-  if (filteredUsers.length === 0) {
-    return 0;
-  }
+  //const filteredUsers = users.filter(user => user.gender === gender);
+  
+  // if (filteredUsers.length === 0) {
+  // return 0;
+  // }
 
-  const sumAge = filteredUsers.reduce((acc, user) => acc + user.age, 0)
+  //const sumAge = filteredUsers.reduce((acc, user) => acc + user.age, 0)
   
-  return sumAge / filteredUsers.length;
+  //return sumAge / filteredUsers.length;
 }
