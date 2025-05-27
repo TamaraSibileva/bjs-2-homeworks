4565fab592a3648a5defb6444b2a2d6392f8f85e
Tamara Sibileva <sibilevato@gmail.com>
2025-05-27 20:45:26 +1200

    Task completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
+  if (!this.marks) {
+    console.log("Ошибка! Студент отчислен.");
+    return 0;
+  }
   
+  this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
+  if (!this.hasOwnProperty('marks')) {
+    return 0;
+  }
+
+  if (!this.marks.length) {
+    return 0;
+  }
   
+  return this.marks.reduce((sum, item, index, arr) => sum + item / arr.length, 0);
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
