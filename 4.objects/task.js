415a9f2964a8e59ca9127d74277526b9a0969768
Tamara Sibileva <sibilevato@gmail.com>
2025-05-27 20:48:23 +1200

    Task changed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,11 +5,11 @@ function Student(name, gender, age) {
   this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
+Student.prototype.setSubject = function(subjectName) {
   this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
+Student.prototype.addMarks = function(...marks) {
   if (!this.marks) {
     console.log("Ошибка! Студент отчислен.");
     return 0;
@@ -18,7 +18,7 @@ Student.prototype.addMarks = function (...marks) {
   this.marks.push(...marks);
 }
 
-Student.prototype.getAverage = function () {
+Student.prototype.getAverage = function() {
   if (!this.hasOwnProperty('marks')) {
     return 0;
   }
@@ -30,7 +30,7 @@ Student.prototype.getAverage = function () {
   return this.marks.reduce((sum, item, index, arr) => sum + item / arr.length, 0);
 }
 
-Student.prototype.exclude = function (reason) {
+Student.prototype.exclude = function(reason) {
   delete this.subject;
   delete this.marks;
   this.excluded = reason;
