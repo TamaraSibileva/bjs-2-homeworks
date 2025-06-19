7da2857b2d22c936b77654ebeb4f42647c4ccaa3
Tamara Sibileva <sibilevato@gmail.com>
2025-06-19 18:45:20 +1200

    Task1 completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,20 @@
-﻿
+﻿//Task1: Number formatter:
+function parseCount(value) {
+  const parsedValue = Number.parseFloat(value);
+
+  if (Number.isNaN(parsedValue)) {
+    throw new Error ("Невалидное значение");
+  }
+
+  return parsedValue;
+}
+
+function validateCount(value) {
+  try {
+    const parsedValue = parseCount(value);
+
+    return parsedValue;
+  } catch (error) {
+      return error;
+  }
+}
